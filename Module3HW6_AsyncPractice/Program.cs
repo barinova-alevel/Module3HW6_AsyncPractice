d737ffd628cf0a120438c971c0a0a5b4a9a68bf6
barinova-alevel <you@example.com>
2021-10-28 09:47:38 +0300

    Task with Fibonacchi

--- Module3HW6_AsyncPractice/Program.cs
+++ Module3HW6_AsyncPractice/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace Module3HW6_AsyncPractice
 {
@@ -6,7 +7,18 @@ namespace Module3HW6_AsyncPractice
     {
         static void Main(string[] args)
         {
-            
+            Task t = Task.Factory.StartNew(() =>
+            {
+                FibonacciSeries(135);
+            });
+            t.Wait(10000);
+        }
+
+        public static int FibonacciSeries(int n)
+        {
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+            return FibonacciSeries(n - 1) + FibonacciSeries(n - 2);
         }
     }
 }
